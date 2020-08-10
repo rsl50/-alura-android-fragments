82bf148e42328bc9ff77307b77954383c30ad006
Robson Lima <rsl.robson@gmail.com>
2020-08-10 16:24:04 -0300

    Ajustando o container conforme orientação da tela

--- android-tech-news-projeto/app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
+++ android-tech-news-projeto/app/src/main/java/br/com/alura/technews/ui/activity/NoticiasActivity.kt
@@ -1,6 +1,7 @@
 package br.com.alura.technews.ui.activity
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -64,8 +65,14 @@ class NoticiasActivity : AppCompatActivity() {
         fragment.arguments = dados
 
         transacaoFragment {
-            addToBackStack(null)
-            replace(R.id.activity_noticias_container_secundario, fragment)
+            val container = if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                R.id.activity_noticias_container_secundario
+            } else {
+                addToBackStack(null)
+                R.id.activity_noticias_container_primario
+            }
+
+            replace(container, fragment)
         }
     }
 
